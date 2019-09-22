12f23529b367d7a352a229683527166fe4a0f9df
Matthew Adams <bookpunk@bookpunk-3.local>
2019-09-22 10:49:33 -0500

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='marsroverphotos',
+      version='0.1',
+      description='Retrieve Mars Rover Photos using the NASA API',
+      url='https://github.com/bookpunk/marsroverphotos.git',
+      author='Matt Adams',
+      author_email='bookpunk@gmail.com',
+      license='MIT',
+      packages=['marsroverphotos'],
+      zip_safe=False)
+
